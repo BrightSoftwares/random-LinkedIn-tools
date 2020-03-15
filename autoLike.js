7708529c348e8d2d655ec692d58b4306796fbb82
fullbright <full3right@gmail.com>
2020-03-15 15:33:49 +0100

    Update autoLike.js
    Support for already clicked buttons

--- autoLike.js
+++ autoLike.js
@@ -1,15 +1,37 @@
-var doc = document;
-var socialBar = doc.getElementsByClassName('feed-shared-social-actions feed-shared-social-action-bar ember-view');
+var socialBar = document.getElementsByClassName('react-button__trigger artdeco-button artdeco-button--muted artdeco-button--4 artdeco-button--tertiary ember-view');
+
+console.log("Social bar is " + socialBar);
+
 function getJobPostingObj(obj, n) {
       setTimeout(() => {
-        doc.getElementById(obj.getAttribute('id')).scrollIntoView();
-        var likeBtn = obj.getElementsByTagName('button')[0];
-		var btnStatus = obj.getElementsByClassName('visually-hidden')[0].innerText;
-		if(btnStatus == 'Like'){
-			likeBtn.click();
+        console.log('Processing object ' + n);
+        var btnViewId = obj.getAttribute('id');
+        console.log('Scrolling to view with ID ' + btnViewId);
+        document.getElementById(btnViewId).scrollIntoView();
+
+        console.log("Getting button text " );
+        var likeBtn = obj;
+        console.log("Like btn = " + likeBtn );
+
+	var btnStatus = likeBtn.innerText;
+        console.log("Button text is " + btnStatus);
+		if(btnStatus == 'J’aime'){
+                        console.log("Found the like button. Checking if it is pressed.");
+
+                        var ariapressed = likeBtn.getAttribute('aria-pressed');
+                        console.log("Aria pressed : " + ariapressed);
+                        if(ariapressed != 'true'){
+                            console.log("Yessss!!!!!!! Ready to click. We can click!");
+			    likeBtn.click();
+                        }
+                        else{
+                            console.log("Already pressed :(");
+                        }
 		}
       }, ((n + 1) * 1800));
     }
-for(i=0; i<1001; i++){
+
+
+for(var i=0; i<socialBar.length; i++){
 	getJobPostingObj(socialBar[i], i);
 }
