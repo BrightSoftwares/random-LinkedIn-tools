c94f972ae349d02efa4ba1d7854a2e25bf465b4a
fullbright <full3right@gmail.com>
2020-03-15 15:54:24 +0100

    Update linkedIn_feed_cleaner.js
    Fix for french interface.

--- linkedIn_feed_cleaner.js
+++ linkedIn_feed_cleaner.js
@@ -1,6 +1,7 @@
 /* This kills promoted items and anything that is not an original post. No more seeing every action your connections make*/
-var domObserver = new MutationObserver(() => {
-  Array.from(document.getElementsByClassName('relative ember-view')).forEach(i=> {if(/>Promoted<|class="feed-shared-header__text-view"/.test(i.innerHTML)){i.outerHTML = '';}});
+var domObserver = new MutationObserver(() => { 
+  console.log("Found one element");
+  Array.from(document.getElementsByClassName('relative ember-view')).forEach(i=> {if(/sponsorisé<|class="feed-shared-text-view"/.test(i.innerHTML)){i.outerHTML = '';}});
 });
 domObserver.observe(document.getElementsByClassName('core-rail')[0], {
   childList: true,
